8f6175b27006139f494a28f860f7a3a84c6ff1c0
Dungha125 <dungha122405@gmail.com>
2026-01-13 14:42:15 +0700

    upda

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,11 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 import App from './App.tsx'
 import './index.css'
 
-const GOOGLE_CLIENT_ID = "347974923411-2ln342c2j6kcv5nc9sngpbqn97suhqhs.apps.googleusercontent.com"
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || "347974923411-2ln342c2j6kcv5nc9sngpbqn97suhqhs.apps.googleusercontent.com"
+
+if (!GOOGLE_CLIENT_ID) {
+  console.error('VITE_GOOGLE_CLIENT_ID is not set. Please configure it in your .env file.')
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
